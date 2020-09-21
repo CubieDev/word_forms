5aaa709b94f62f5943ad417cd563afd231d2f726
CubieDev <cubiegamedev@gmail.com>
2020-09-21 12:40:59 +0200

    Slight improvements to lemmatizer
    No longer sort twice, use set to remove duplicates for faster sorting, only sort if nonempty.

--- word_forms/lemmatizer.py
+++ word_forms/lemmatizer.py
@@ -5,12 +5,10 @@ def lemmatize(word):
     """
     Out of all the related word forms of ``word``, return the smallest form that appears first in the dictionary
     """
-    forms = [word for pos_form in get_word_forms(word).values() for word in pos_form]
-    forms.sort()
-    forms.sort(key=len)
-    try:
-        return forms[0]
-    except IndexError:
+    forms = {word for pos_form in get_word_forms(word).values() for word in pos_form}
+    if forms:
+        return sorted(forms, key=len)[0]
+    else:
         raise ValueError("{} is not a real word".format(word))
 
 
